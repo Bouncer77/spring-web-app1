19517587825aaad29ac0d6e5e60b67d26ca53e53
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-09-03 01:28:25 +0300

    lesson 19

--- src/main/java/com/bouncer77/springmvc/controller/FirstController.java
+++ src/main/java/com/bouncer77/springmvc/controller/FirstController.java
@@ -3,22 +3,33 @@ package com.bouncer77.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Kosenkov Ivan
  * Created by Kosenkov Ivan on 02.09.2020
  */
 @Controller
-// k@RequestMapping("first")
+@RequestMapping("first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    public String goodByePage(@RequestParam("name") String name,
+                              @RequestParam("surname") String surname) {
+        System.out.println("Goodbye, " + name + " " + surname);
         return "first/goodbye";
     }
 
