2050fc333e7039ff348c75d19fb271e147d5bded
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-09-03 01:57:34 +0300

    lesson 20

--- src/main/java/com/bouncer77/springmvc/controller/FirstController.java
+++ src/main/java/com/bouncer77/springmvc/controller/FirstController.java
@@ -1,6 +1,7 @@
 package com.bouncer77.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,6 +32,48 @@ public class FirstController {
                               @RequestParam("surname") String surname) {
         System.out.println("Goodbye, " + name + " " + surname);
         return "first/goodbye";
+    }
+
+    @GetMapping("/hi")
+    public String hiPage(@RequestParam("name") String name,
+                         @RequestParam("surname") String surname,
+                         Model model) {
+        model.addAttribute("key1", "Hi, " + name + " " + surname);
+        return "first/hi";
+    }
+
+    @GetMapping("/calc")
+    public String culcPage(@RequestParam("a") int a,
+                           @RequestParam("b") int b,
+                           @RequestParam("act") String act,
+                           Model model) {
+
+        System.out.println(a + " " + act + " " + b + " = ");
+
+        int res = 0;
+        switch (act) {
+            case "mul":
+                res = a * b;
+                break;
+            case "add":
+                res = a + b;
+                break;
+            case "sub":
+                res = a - b;
+                break;
+            case "div":
+                if (b != 0) {
+                    res = a / b;
+                } else {
+                    res = -9999;
+                }
+                break;
+            default:
+                res = 1;
+        }
+        model.addAttribute("res", res);
+
+        return "first/calc";
     }
 
 }
